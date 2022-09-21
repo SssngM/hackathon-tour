c973a6aad340727182c35a9501c200add09fcb9f
SssngM <rarang222@gmail.com>
2022-09-21 15:12:37 -0700

    made title

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1> Take you to HIT place</h1>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+<script>
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
