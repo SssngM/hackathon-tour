e2bf9c387667f2df873395ad58b3e829d7b99d38
ROSSROSALES <ross.erosales@gmail.com>
2022-09-22 10:59:29 -0400

    Book Api remove API Key

--- src/BookApi.js
+++ src/BookApi.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const bookdata = () => {
-  axios.defaults.headers['X-API-KEY'] = 'AIzaSyAvm5gaFT75cYTXdirFP3QPu_1sHGMkufk';
-  return axios.get("https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor")
+  return axios.get("https://www.googleapis.com/books/v1/volumes?q=flowers")
     .then((response) => response);
 }
